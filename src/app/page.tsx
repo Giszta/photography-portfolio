e7c49f21ff76cf575315c1ec9684af5b82a08586
AdamGiszter <a.m.giszter@gmail.com>
2024-03-21 22:02:38 +0100

    mobile navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import Circles from "./HomePage/Circles";
 import HomePhotoSlide from "./HomePage/HomePhotoSlide";
 import Footer from "./components/Footer/Footer";
 import Navbar from "./components/Navbar/Navbar";
+import NavbarMobile from "./components/Navbar/Navbar-mobile";
 
 export default function Home() {
 	return (
 		<main>
 			<Navbar />
+			<NavbarMobile />
 			<HomePhotoSlide />
 			<Circles />
 			<Footer />
