86d6c5dd34265b0b424df65020235b3d8c060551
AdamGiszter <a.m.giszter@gmail.com>
2024-03-24 16:10:52 +0100

    qoute and author circle

--- src/app/HomePage/QuoteCircle.tsx
+++ src/app/HomePage/QuoteCircle.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function QuoteCircle() {
-	return <div>QuoteCircle</div>;
+	return (
+		<div className="animate-fade-left animate-ease-in bg-white bg-opacity-60 relative right-3 flex w-52 h-52 rounded-full justify-center items-center ">
+			<div className="text-black  text-center ">
+				Problemem fotoamatora jest to, że nie ma on powodów do robienia zdjęć.
+			</div>
+		</div>
+	);
 }
