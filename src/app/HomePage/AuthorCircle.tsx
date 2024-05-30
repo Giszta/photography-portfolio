349dad5cb04f057c63fd256e8412bbb1aebc7ed2
AdamGiszter <a.m.giszter@gmail.com>
2024-05-30 14:15:24 +0200

    animations added

--- src/app/HomePage/AuthorCircle.tsx
+++ src/app/HomePage/AuthorCircle.tsx
@@ -2,8 +2,11 @@ import React from "react";
 
 export default function AuthorCircle({ authorName }: { authorName: string }) {
 	return (
-		<div className="bg-black bg-opacity-65 relative left-3 flex w-52 h-52 rounded-full justify-center items-center  animate-fade-right animate-ease-in">
-			<div className="text-white text-center px-5 ">{authorName}</div>;
+		<div className="bg-black bg-opacity-65 relative left-3 flex w-60 h-60 rounded-full justify-center items-center  animate-fade-right animate-ease-in">
+			<div className="text-white text-center px-5 font-serif text-lg">
+				{authorName}
+			</div>
+			;
 		</div>
 	);
 }
