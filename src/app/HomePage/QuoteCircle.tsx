b8718a69f09d1c209d3eddf46f8476473ed91c21
AdamGiszter <a.m.giszter@gmail.com>
2024-05-30 07:54:22 +0200

    visual adjustments

--- src/app/HomePage/QuoteCircle.tsx
+++ src/app/HomePage/QuoteCircle.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function QuoteCircle({ quoteText }: { quoteText: string }) {
 	return (
-		<div className="animate-fade-left animate-ease-in bg-white bg-opacity-60 relative right-3 flex w-52 h-52 rounded-full justify-center items-center ">
-			<div className="text-black  text-center ">{quoteText}</div>
+		<div className="animate-fade-left animate-ease-in bg-white bg-opacity-65 relative right-3 flex w-52 h-52 rounded-full justify-center items-center ">
+			<div className="text-black  text-center px-5">{quoteText}</div>
 		</div>
 	);
 }
--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -13,7 +13,7 @@ export default function Navbar() {
 	};
 
 	return (
-		<nav className="lg:bg-black opacity-75 absolute w-screen">
+		<nav className="lg:bg-black opacity-75 absolute w-screen shadow-lg shadow-stone-500/50">
 			<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
 				<div className="flex items-center justify-between h-20">
 					<div className="flex items-center">
