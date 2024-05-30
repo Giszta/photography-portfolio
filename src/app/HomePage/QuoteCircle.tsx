b0c7106a07a620e5ed850bbbd45a1eb28594e214
AdamGiszter <a.m.giszter@gmail.com>
2024-05-30 14:15:24 +0200

    animations added

--- src/app/HomePage/QuoteCircle.tsx
+++ src/app/HomePage/QuoteCircle.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function QuoteCircle({ quoteText }: { quoteText: string }) {
 	return (
-		<div className="animate-fade-left animate-ease-in bg-white bg-opacity-65 relative right-3 flex w-52 h-52 rounded-full justify-center items-center ">
-			<div className="text-black  text-center px-5">{quoteText}</div>
+		<div className="animate-fade-left animate-ease-in bg-white bg-opacity-65 relative right-3 flex w-60 h-60 rounded-full justify-center items-center ">
+			<div className="text-black  text-center px-5 font-serif italic text-sm">
+				{quoteText}
+			</div>
 		</div>
 	);
 }
